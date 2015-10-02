03cbf76bc3f69eede887dc7c46e6605381b4bb68
Jaimedgp <jaime.diez.gp@gmail.com>
2015-10-02 20:14:47 +0200

    First Python utility
    reads from input, outputs to file with format(*.ods extension)

---
+++ test1.py
@@ -0,0 +1,10 @@
+nombre = raw_input("¿Cual es tu nombre?")
+mision = raw_input("¿cual es tu mision en la vida?")
+Color = raw_input("¿Cual es tu color favorito?")
+
+print "Entonces tu nombre es %s, tu mision en la vida es %s y tu color favorito es %s." % (nombre, mision,Color)
+
+fl = open('prueba.ods','w')
+for i in range(1000):
+    fl.write(nombre+"\t"+mision+"\t"+Color+"\n")
+fl.close()
