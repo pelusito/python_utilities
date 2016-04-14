7a13a96c0a73f77146b15d698284c9420681b1d4
Jaime Díez González-Pardo <jaime.diez.gp@gmail.com>
2016-04-14 20:55:10 +0200

    Added files via upload

--- Script.py
+++ Script.py
@@ -2,98 +2,103 @@ from math import sqrt, sin, pi, degrees, radians
 import numpy as np
 import matplotlib.pyplot as plt
 
-Titulo = str(raw_input('Titulo: '))
+Title = str(raw_input('Title: '))
 
-Xname = str(raw_input('Variable dependiente: '))
-Xmed = str(raw_input('Unidades: '))
+Xname = str(raw_input('Dependent variable: '))
+Xmed = str(raw_input('Units: '))
 
 x = (raw_input('x= '))
 
-Mx = np.matrix([])
+Mx = np.array([])
 
 boolone = True
 
 while boolone :
-	Mx = np.matrix ([Mx, float(x)])
+	Mx = np.insert(Mx, Mx.size, float(x)) #kdvsjnlsnak
 	x = (raw_input('x= '))
-	if str(x) == 'fin':
+	if str(x) == 'End':
 		boolone = False
 	else:
 		boolone = True
 
 
-Yname = str(raw_input('Variable independiente: '))
-Ymed = str(raw_input('Unidades: '))
+Yname = str(raw_input('independent variable: '))
+Ymed = str(raw_input('Units: '))
 y = (raw_input('y= '))
 
-My = np.matrix([])
+My = np.array([])
 
 booltwo = True 
 
 while booltwo :
-	My = np.matrix ([My, float(y)])
+	My = np.insert(My, My.size, float(y))
 	y = (raw_input('y= '))
-	if str(y) == 'fin':
+	if str(y) == 'End':
 		booltwo = False
 	else:
 		booltwo = True
 
 if Mx.size != My.size:
-	print 'NO HAY EL MISMO NUMERO DE DATOS DE %s QUE DE %s' %(Xname, Yname)
+	print 'THERE ARE NOT THE SAME NUMBER OF MEASURES OF %s THAN  %s' %(Xname, Yname)
 
 else:
-	class Grafica(object):
+	class Graphics(object):
 
 		#docstring for Grafica#
 		VX = Mx
 		VY = My
 
 		
-		def guardar(self):
+		def save(self):
 			w = 0
-			fl = open('/home/jaime/Escritorio/Table.ods', 'w')
-			for i in range(Mx.size):
+			fl = open('/home/jaime/Escritorio/'+Title+'.ods', 'w')
+			for x in xrange(1):
 				fl.write(Xname+'/'+Xmed+ "\t" + Yname+'/'+Ymed + "\n")
-				fl.write((str(Mx.item(w)) + "\t" + str(My.item(w)) + "\n"))
-				w += 1
-			fl.close()
+				for i in range(Mx.size):
+					fl.write((str(Mx.item(w)) + "\t" + str(My.item(w)) + "\n"))
+					w += 1
+				fl.close()
 
-		def pendiente(self):
+		def slope(self):
 
 			x_median = float(Mx.sum()) / float(Mx.size)
-			x_i = Mx - x_media
+			x_i = Mx - x_median
 			a = x_i * My
 			xx_i = x_i**2
 			m = float(a.sum()) / float(xx_i.sum())
 			return m
 
-		def ordenada(self):
+		def intercept(self):
 			Mxx = Mx**2
 			Mxy = Mx*My
 			qwerty = (float(My.sum())*float(Mxx.sum())-float(Mx.sum())*float(Mxy.sum())) / (float(Mx.size)*float(Mxx.sum()) - (float(Mx.sum()))**2)
 			return qwerty
 
-		def grafica(self):
+		def graph(self):
 
-			xes = range(Mx.min(),Mx.max(), ((Mx.max()-Mx.min())/100))
+			xes = np.arange(Mx.min(),Mx.max(), ((Mx.max()-Mx.min())/100))
 
-			yes = Grafica().pendiente()*xes + Grafica().ordenada()
-
-			f2 = plt.figure(figsize=(12.0, 20.0))
+			yes = Graphics().slope()*xes + Graphics().intercept()
 
 			plt.figure(1)
-			plt.subplot(211)
+			plt.subplot(211) 
 			plt.plot(Mx, My, 'ro', xes, yes, 'r')
-			plt.axis([Mx.min()])
-			plt.title(Titulo)
+			plt.axis([Mx.min(), Mx.max(), My.min(), My.max()])
+			plt.title(Title)
 			plt.xlabel(Xname)
 			plt.ylabel(Yname)
 			plt.show()
 
+			f2 = plt.figure(figsize=(12.0, 20.0))
+
 		def medianX(self):
-			return float(Mx.sum() / float(Mx.size))
+			p = np.sum(Mx)
+			u = Mx.size
+			print p/u
 
 		def medianY(self):
-			return float(My.sum()) / float(My.size)
+			p = np.sum(My)
+			u = My.size
+			print p/u
 
-print Grafica().medianX()
+Graphics().save()
