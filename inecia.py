228aabe393a2aa13c2b8b691373318815b30caa6
jaimedgp <jaime.diez.gp@gmail.com>
2016-10-21 13:44:20 +0200

    A little program

---
+++ inecia.py
@@ -0,0 +1,18 @@
+from math import asin, pi
+
+h = [6.4, 14.7, 22.2, 29.9, 37.6, 45.2, 52.6, 60, 69.8, 77.1, 84.4, 91.7]
+hA = []
+alpha = []
+alphag =[]
+
+for i in range(len(h)):
+	hA.append(h[i]+1.95)
+	alpha.append(asin(hA[i]/100.2))
+	alphag.append(alpha[i]*180/(pi))
+
+fl = open('C:\Users\jaime\Desktop\Inercia\sabla.csv', 'w') # open and create an .ods file
+for x in xrange(1): # loop to write the file
+	fl.write('title 1' + '\t' + 'title 2' + '\t' + 'title 2'+ '\t' + 'title 3' + "\n") # write a header
+	for i in range(len(h)): # loop to write element by element to the file
+		fl.write(str(h[i]) + "\t"+ str(hA[i]) + "\t" + str(alpha[i])+ "\t" + str(alphag[i])+ "\n") # write in two colums
+fl.close() # close file
